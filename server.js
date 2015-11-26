3d052b664a1e9a83fd4ccba92bc772fc5993707f
rs91092 <liharsharma91092@gmail.com>
2015-11-26 23:14:08 +0530

    getting Delhi Election Data

--- server.js
+++ server.js
@@ -1,3 +1,37 @@
 /**
  * Created by rs on 25/11/15.
  */
+var express = require('express');
+var fs = require('fs');
+var request = require('request');
+var cheerio = require('cheerio');
+var app     = express();
+var rootUrl='http://myneta.info/';
+var options = {
+    url:rootUrl,
+    headers: {
+        'User-Agent': 'Mozilla'
+    }
+};
+
+request(options, function callback(error, response, body) {
+        // console.log('cm here');
+        $ = cheerio.load(body);
+        var l=$('.item').children().closest('a');
+//console.log(l.length);
+  //    console.log(l[0].children[0].data);
+    //console.log(l[0].attribs.href)
+    for(var ii=0;ii< l.length ; ii++){
+        if(l[ii].children[0].data == "Delhi"){
+
+        }
+    }
+        //if (kk=chngvr.length-1){
+        //   //callback function
+        //    console.log(kk);
+        //  printglobalArray();
+        //}
+
+    }
+
+);
